d6dd26515eb76c255f6e054ce17b7b278f873b42
Sushama Jarange <sushamajarange@gmail.com>
2015-12-25 18:20:53 +0800

    Added Sample.java program

---
+++ Sample.java
@@ -0,0 +1,19 @@
+//class is keyword used to declare a class in java
+// public is keyword and also access modifier to represent the visibility 
+
+public class Sample {
+
+/* if we use static method then no need to create an object. The main method is executed by JVM(JVM sends java
+ bytecode to the processor) so no need to  create an object */	
+// void is return type of method and it does'nt return any value
+// main is used for starting the program
+// String[] args is used for command line argument
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+// System.out.println is used print the statement.
+		System.out.println("Hey buddy ..");
+	}
+
+}
---
+++ Table.java
@@ -0,0 +1,21 @@
+public class Table {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=2;i<=20;i=i+2){
+			System.out.println(i);
+		}
+	}
+}
+/* 'public static void main String[] args' is same as I mentioned in Sample.java
+ * In this Table program it is printing table of 2
+ * 
+ * Execution -
+ * 1st it create a variable of name i
+ * 2nd - go inside the for loop (i=2) check if  i<=20, if it is yes then go inside the loop (if i~=20 then exit the loop)
+ * 3rd - print i=2
+ * 4th - goto loop, increment i by 2 check if it is less than 20 go inside and print 4
+ * 5th - again goto loop, increment i by 2 check if it is less than 20 go inside and print 6
+ * 6th - And lly execute it till i<=20, once i>20 then exit the loop 
+ * And it will print 2's table
+*/
