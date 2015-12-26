9b1a321e3d5a6482eca3187d1fee31aef6689fe6
Sushama Jarange <sushamajarange@gmail.com>
2015-12-27 01:35:40 +0800

    Added new changes

--- SumEmployeeTable/Sum.java
+++ SumEmployeeTable/Sum.java
@@ -1,14 +1,18 @@
-
 public class Sum {	
 	public static void main(String[] args) {
-		String s1 = args[0];				// It takes an argument from command line 
-		int i = Integer.parseInt(s1);		// It converts string (s1) to an integer  
-		
-		String s2 = args[1];				// It takes an argument from command line 
-		int j = Integer.parseInt(s2);		// It converts string (s2) to an integer  
-		
-		int  sum = i + j;					// Make sum of entered numbers
-		System.out.println("Sum = "+sum);	// print sum
+		// try-catch is used for catch an exception
+		try{
+			String s1 = args[0];				// It takes an argument from command line 
+			int first = Integer.parseInt(s1);		// It converts string (s1) to an integer  
+			
+			String s2 = args[1];				// It takes an argument from command line 
+			int second = Integer.parseInt(s2);		// It converts string (s2) to an integer  
+			
+			int  sum = first + second;					// Make sum of entered numbers
+			System.out.println("Sum of entered two numbers = "+sum);	// print sum of 2 numbers
+		}	
+		catch(NumberFormatException e){
+			System.out.println("Number format exception... Please enter number");
+		}
 	}
-
 }
--- SumEmployeeTable/Table.java
+++ SumEmployeeTable/Table.java
@@ -1,12 +1,16 @@
 class Table {
 	public static void main(String[] args) {
-		String s = args[0];				// It takes an argument from command line 
-		int i = Integer.parseInt(s);	// // It converts string (s) to an integer 
-		int j, tab=0;			
-		for(j=0;j<10;j++){			// This for loop will execute 10 times, once j=10 it will exit automatically
-			tab = i + tab;			// This is printing the the sum	
-			System.out.println(tab);	// This will print Table of entered number
+		// try-catch is used for catch an exception
+		try{
+			String s = args[0];				// It takes an argument from command line 
+			int number = Integer.parseInt(s);	// It converts string (s) to an integer  
+			for(int j=1;j<=10;j++){				// This for loop will execute 10 times, once j=11 it will exit automatically
+				System.out.println(j*number);	//Print table
+			}
 		}
-		
+		// If user entered other than number it will catch the exception and show the message to user
+		catch(NumberFormatException e){
+			System.out.println("Number format exception... Please enter number");
+		}
 	}
 }
