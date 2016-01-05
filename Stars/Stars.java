c9ba622445c7ed09544fbb73a2295f18ae056049
Sushama Jarange <sushamajarange@gmail.com>
2016-01-06 03:00:45 +0800

    Stars program added

---
+++ Stars/Stars.java
@@ -0,0 +1,20 @@
+
+public class Stars {
+	public static void main(String[] args) {
+		int i,j;
+		for(i=1;i<=5;i++){
+			System.out.println();
+			for(j=1;j<=i;j++){
+				System.out.print(" * ");
+			}
+		}
+	}
+}
+/*
+ Output :
+   * 
+   * 
+   *  * 
+   *  *  * 
+   *  *  *  *
+*/
