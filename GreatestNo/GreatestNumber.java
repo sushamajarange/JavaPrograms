1e1f61abb2573070e55cd2fb2eb45624dd432822
Sushama Jarange <sushamajarange@gmail.com>
2016-01-12 02:30:59 +0800

    Added greatest number program with changes

--- GreatestNo/GreatestNumber.java
+++ GreatestNo/GreatestNumber.java
@@ -1,29 +1,33 @@
 public class GreatestNumber {
-	 public static int k=3;
-	public int main(int[] args){
-		return 0;
-	}
-	void temp(){
-		int k=4;
-		System.out.println(k);
-	}
 	public static void main(String[] args) {
-		GreatestNumber obj=new GreatestNumber();
-		int i,j;System.out.println(k);
-		obj.temp();
-		i=obj.main(args[0]);
-		j=obj.main(args[1]);
-		if(i<j){
-			System.out.println("First number is greater from those numbers which you entered");
+		try{
+			if(args[0].length()==0){
+					System.out.println("Please enter numbers");
+			}
+			if(args.length==2){
+				int i,j;
+				i=Integer.parseInt(args[0]);
+				j=Integer.parseInt(args[1]);
+				if(i>j){
+					System.out.println("First number is greater : "+i);
+				}
+				else{
+					System.out.println("Second number is greater : "+j);
+				}
+			}
+			else{
+				System.out.println("Please enter only two numbers");
+			}
 		}
-		else{
-			System.out.println("Second number is greater from those numbers which you entered");
+		
+		catch(ArrayIndexOutOfBoundsException aie){
+			System.out.println("Please enter numbers");
 		}
+		catch(NumberFormatException nfe){
+			System.out.println("Please Enter number not a character");
+		}
+		
 	}
-	private int main(String string) {
-		// TODO Auto-generated method stub
-		return 0;
-	}	
 }
 
 
