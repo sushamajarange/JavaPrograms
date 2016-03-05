76fdf640236af6c978d180fcc3a4fff756d100ff
Sushama Jarange <sushamajarange@gmail.com>
2016-03-05 14:39:03 +0800

    Switch Programs added

---
+++ Chapter6/SwitchEgIntType.java
@@ -0,0 +1,25 @@
+
+public class SwitchEgIntType {
+
+	public static void main(String[] args) {
+		int x =3;
+
+		switch (x){
+		case 1:
+			System.out.println("x is 1");
+			break;
+		case 2:
+			System.out.println("x is 2");
+			break;
+		case 3:
+			System.out.println("x is 3");
+			break;	
+		default:
+			System.out.println("default case");
+			break;
+		}
+
+	}
+
+}
+
---
+++ Chapter6/SwitchEgStr.java
@@ -0,0 +1,24 @@
+
+public class SwitchEgStr {
+
+	public static void main(String[] args) {
+		String str="sush";
+		switch (str.length()){
+		case 1:
+			System.out.println("x is 1");
+			break;
+		case 2:
+			System.out.println("x is 2");
+			break;
+		case 3:
+			System.out.println("x is 3");
+			break;	
+		default:
+			System.out.println("default case");
+			break;
+		}
+
+	}
+
+}
+
