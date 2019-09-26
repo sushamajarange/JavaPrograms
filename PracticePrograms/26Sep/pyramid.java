5c295132ad79332e6ecd94af710dd740c1a5cef5
Vishnu P <vishnu@mobantica.com.sg>
2019-09-27 00:51:36 +0800

    Pyramid of stars

---
+++ PracticePrograms/26Sep/pyramid.java
@@ -0,0 +1,24 @@
+
+public class pyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i<=4; i++) {
+			for(int j = 5; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+/*
+---------- Output of the pyramid is below----------
+*****
+****
+***
+**
+*
+---------------------------------------------------
+
+*/
