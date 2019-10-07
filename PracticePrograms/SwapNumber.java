1e4b69fc423e445ae8d6ca0595d0428274535714
Sushama Jarange <sushamajarange@gmail.com>
2019-10-07 15:35:50 +0800

    Swap two numbers

---
+++ PracticePrograms/SwapNumber.java
@@ -0,0 +1,16 @@
+
+public class SwapNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10, b=20;
+		System.out.println("Before : \na : "+a+" b : "+b);
+		int temp;
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("After : \na : "+a+" b : "+b);
+
+	}
+
+}
