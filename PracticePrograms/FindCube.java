ffbc6b0543bd19ac9f583c0f226fd3c4b3bb1c82
Sushama Jarange <sushamajarange@gmail.com>
2019-10-09 19:01:23 +0800

    Finds cube of entered number

---
+++ PracticePrograms/FindCube.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FindCube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int result = num * num * num;
+		System.out.println("Cube of entered number is : "+result);
+	}
+
+}
