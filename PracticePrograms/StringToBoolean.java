693befab522f9e138f19beca9f9c3a718c884612
Sushama Jarange <sushamajarange@gmail.com>
2019-10-10 19:18:08 +0800

    Converts string to boolean

---
+++ PracticePrograms/StringToBoolean.java
@@ -0,0 +1,20 @@
+
+public class StringToBoolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String st1 = "false";
+		String st2 = "Something";
+		String st3 = "true";
+		
+		boolean b1 = Boolean.parseBoolean(st1);
+		boolean b2 = Boolean.parseBoolean(st2);
+		boolean b3 = Boolean.parseBoolean(st3);
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		
+	}
+
+}
