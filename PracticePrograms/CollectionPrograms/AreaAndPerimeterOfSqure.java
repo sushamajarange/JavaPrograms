02bf83b3c6022c6cdab0f40a4c8b626f989b7a15
Sushama Jarange <sushamajarange@gmail.com>
2019-10-22 16:50:34 +0800

    finds area and perimeter of a squre

---
+++ PracticePrograms/CollectionPrograms/AreaAndPerimeterOfSqure.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AreaAndPerimeterOfSqure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		int r = sc.nextInt();
+		
+		float pi =3.14F;
+		
+		System.out.println(pi*r*r);
+	}
+
+}
